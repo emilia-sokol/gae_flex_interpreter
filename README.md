75ee0706d87773f899b78562010716704810221a
Emilia Sokół <emilia.sokol94@gmail.com>
2020-07-12 14:09:47 +0200

    R interpreter

---
+++ README.md
@@ -0,0 +1,18 @@
+# GAE Flex Interpreter
+Interpreter module for Internet of Things Ecosystem (R and java)
+
+
+#### 1. Install
+```cmd
+> pip install -r requirements.txt
+```
+
+
+#### 2. Run
+
+to run from terminal:
+```cmd
+> python main.py
+```
+
+run on address: http://127.0.0.1:9090
--- main.py
+++ main.py
@@ -15,8 +15,15 @@
 # [START gae_flex_quickstart]
 import logging
 
+from rpy2 import robjects
 from flask import Flask
+from flask import request
 
+import subprocess
+from flask import jsonify
+from rpy2.robjects import pandas2ri
+import numpy
+import json
 
 app = Flask(__name__)
 
@@ -25,6 +32,87 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
+
+
+def is_nd_array(obj):
+    if isinstance(obj, numpy.ndarray):
+        return obj.tolist()
+    else:
+        return obj
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    pandas2ri.activate()
+    """Process the uploaded file and upload it to Google Cloud Storage."""
+    print(request.form)
+    mapper_str: str = request.form['mapper']
+    entity = request.form['entity']
+
+
+    if not mapper_str:
+        return 'No file uploaded.', 400
+
+    mapper = robjects.r(mapper_str)
+    result = mapper(int(entity))
+
+    print(result)
+    # test
+    # b = dict(zip(result.names, result[0]))
+    # print(b)
+    # b = result.tolist()
+    # print(b)
+    # print(type(b))
+    # test end
+
+
+    # @FIXME this is a workaround, result data shouldn't be processed here
+    # but it is somehow required to get result into python data structure from R
+    # in order to then send it through REST API - it needs to be in a json parsable form
+
+    # this will probably work only for the one example I attached to the project
+    # I think about this as a first attempt to create another language interpreter
+    # this needs more investigation
+
+    # possibly we should also send a python function from original project
+    # that will convert result data from R function
+    data = dict(zip(result.names, entity))
+
+    # this is not working because result[0] is int32 type, not int which
+    # makes data object (dict) not processable into json
+    # data = dict(zip(result.names, result[0]))
+    print(data)
+
+    print("Return string value parsable to json")
+    # app_json = json.dumps(data)
+    # print(app_json)
+    # print(type(app_json))
+    return data, 200
+
+
+@app.route('/upload_reducer', methods=['POST'])
+def upload_reducer():
+    pandas2ri.activate()
+    """Process the uploaded file and upload it to Google Cloud Storage."""
+    print(request.form)
+    reducer_str: str = request.form['reducer']
+    values = request.form['values']
+    
+    print(values)
+    print("HOOO")
+
+
+    if not reducer_str:
+        return 'No file uploaded.', 400
+
+    reducer = robjects.r(reducer_str)
+    result = reducer(values)
+
+    print(result)
+   
+    # data = dict(zip(result.names, entity))
+
+    
+    return 200
 
 
 @app.errorhandler(500)
